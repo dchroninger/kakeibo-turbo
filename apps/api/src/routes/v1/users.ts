edf64ac7708484b2a3d711a5750830ada210c5a7
Dave Chroninger <david.a.chroninger@gmail.com>
2023-05-28 17:45:14 -0700

    Completed first pass of user route

--- apps/api/src/routes/v1/users.ts
+++ apps/api/src/routes/v1/users.ts
@@ -1,30 +1,40 @@
 import {Router} from 'express'
-import {User} from 'types'
+import { v4 as uuidv4 } from 'uuid';
+
+import { TypeWithoutId, User } from 'types';
+
+import { _users } from '../../_mock/user';
 
 const router = Router();
 
-const users: User[] = [
-  {id: '1', name: 'John', email: "john@email.com", familyId: null},
-  {id: '2', name: 'Jane', email: "jane@email.com", familyId: "123-123-123"},
-  {id: '3', name: 'Dave', email: "dave@email.com", familyId: null}
-]
-
+// Get all users
 router.get('/', (_, res) => {
-  res.send(users)
+  res.send(_users)
 })
 
+// Create a new user
 router.post('/', (req, res) => {
-  const user = req.body as User
-  users.push(user)
-  res.send(users.find(u => u.id === user.id))
+  const userReq = req.body as TypeWithoutId<User>;
+
+  const user = {
+    id: uuidv4(),
+    ...userReq
+  } as User;
+
+  _users.push(user);
+
+  res.status(201).location(`http://localhost:3001/v1/users/${user.id}`).send(user)
 })
 
+// Get a user by id
 router.get('/:id', (req, res) => {
-  const user = users.find(user => user.id === req.params.id)
+  const user = _users.find(user => user.id === req.params.id)
+
   if (user) {
     res.send(user)
   } else {
     res.status(404).send({error: 'User not found'})
   }
 })
+
 export default router;
